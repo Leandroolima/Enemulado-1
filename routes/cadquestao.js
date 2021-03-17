e1a067f90885b79bc15c6e52fe1f82ff1754c179
Leandroolima <76533497+Leandroolima@users.noreply.github.com>
2021-03-17 15:46:22 -0300

    Update cadquestao.js

--- routes/cadquestao.js
+++ routes/cadquestao.js
@@ -1,28 +1,74 @@
+
 const express = require("express");
 
 const router = express.Router();
-const Post = require("../modules/Mquestao");
+const Post = require("../modules/nomes");
 
 
-router.post('/', async (req, res) => {
 
-    console.log("sa bosta")
+router.get('/', async (req, res) => {
+    try {
+    
 
+        res.sendFile(__basedir + '/index.html')
+
+    } catch (err) {
+        res.json({ message: err });
+    }
+
+});
+
+router.post('/alunopost', async (req, res) =>
+{
+    console.log(req.body.enunciado);
+    console.log(req.body.letraA);
+    
+  
     const post = new Post({
-
-        Enuciado: req.body.Enuciado,
+        enunciado: req.body.enunciado,
         letraA: req.body.letraA,
         letraB: req.body.letraB,
         letraC: req.body.letraC,
         letraD: req.body.letraD,
-        letraE: req.body.letraE
-
+        letraE: req.body.letraE,
+        Resposta: req.body.Resposta
+     
 
     });
-    try {
+    try{
         const savedPost = await post.save();
-        
+        //res.json(savedPost);
         res.sendFile(__basedir + '/index.html')
+    }catch(err){
+        res.json({message: err});
+    }
+
+});
+
+
+router.get('/all', async (req, res) => {
+    try{
+        const respostas = await Post.find();
+        res.json(respostas);
+    }catch(err) {
+        res.json({message: err});
+    }
+});
+
+
+
+router.get('/buscaAluno/:cadastro', async (req, res) => {
+    try {
+        //Post.findOne({ matricula: req.body.matricula })
+       // const post = await Post.findById(req.body.matricula);
+      // console.log( req.params);
+       console.log( req.params.cadastro)
+      const post = await Post.findOne({ cadastro: req.params.cadastro });
+    // console.log(req.body.matricula);
+     //const post = await Post.findOne({ matricula: req.body.matricula });
+
+        res.json(post);
+
     } catch (err) {
         res.json({ message: err });
     }
@@ -30,24 +76,9 @@ router.post('/', async (req, res) => {
 });
 
 
-
-//all
-router.get('/', async (req, res) => {
-
-    try {
-        const posts = await Post.find();
-        res.json(posts);
-
-    } catch (err) {
-        res.json({ message: err });
-
-    }
-});10
-2
-
-
-
 module.exports = router;
+
+
 
 
 
